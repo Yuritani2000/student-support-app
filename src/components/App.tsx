8d43500d0d55c6c35fd2bc23eaad087f90a5b0ea
b1019116 <b1019116@fun.ac.jp>
2021-05-05 17:32:42 +0900

    認証機能の実装

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,28 +21,16 @@ import Auth from './Auth';
 const App: React.FC = () =>{
 
   useEffect(()=>{
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
   });
 
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/frame1' component={Frame1}/>
-        <Route path='/frame2' component={Frame2}/>
         <Route path='/frame5' component={Frame5}/>
-        <Route path='/frame7' component={Frame7}/>
-        <Route path='/frame8' component={Frame8}/>
-        <Route path='/frame9' component={Frame9}/>
-        <Route path='/frame4' component={Frame4}/>
-        <Route path='/frame13' component={Frame13}/>
-        <Route path='/frame16' component={Frame16}/>
-        <Route path='/frame17' component={Frame17}/>
-        <Route path='/frame11' component={Frame11}/>
-        <Route path='/frame15' component={Frame15}/>
-        <Route path='/frame10' component={Frame10}/>
-        <Route path='/frame14' component={Frame14}/>
-        <Route path='/frame3' component={Frame3}/>
-        <Route path='/' component={Auth}/>
+        <Auth>
+        </Auth>
       </Switch>
     </BrowserRouter>
   );
