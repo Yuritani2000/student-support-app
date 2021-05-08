af607d0bb218def8de53ced820bf96459c998797
b1019116 <b1019116@fun.ac.jp>
2021-05-08 14:53:27 +0900

    実装中（エラー）

--- src/components/Frame8.tsx
+++ src/components/Frame8.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AbsoluteBox, FlexBox, HoverElement, RelativeBox, StyledButton, StyledDiv, StyledText } from './StyledComponents';
 import Frame9 from './Frame9';
+import firebase, { database } from '../firebase';
+import { OneSubjectDataType } from '../DataTypes/SubjectTypes';
 
 const Frame8:React.FC = () => {
     const [ isOpeningFrame9, setIsOpeningFrame9 ] = useState(false);
+    const [ subjectList, setSubjectList ] = useState([] as OneSubjectDataType[]);
+
+    const subjectRef = database.ref('subject');
 
     const openFrame9 = () => {
         setIsOpeningFrame9(true);
@@ -14,6 +19,28 @@ const Frame8:React.FC = () => {
     }
 
     const mockSubjects = ['現代文', '古文', '漢文', '物理', '化学', '生物', '数学IA', '数学IIB', '数学III', '地理A', '地理B', '世界史A', '世界史B', '日本史A', '日本史B', '現代社会', '倫理', '家庭', '体育', '保健', '情報科学', 'アルゴリズムとデータ構造', 'プロジェクト学習'];
+
+    useEffect(()=>{
+        const user = firebase.auth().currentUser;
+        if(!user) return;
+        const userId = user.uid;
+        if(!userId) return;
+        const subjectListRef = subjectRef.child(userId);
+        if(!subjectListRef) return;
+        subjectListRef.on('value', (snapshot) => {
+            const subjects = snapshot.val();
+            if(subjects === null) return;
+            const entries = Object.entries(subjects);
+            const gainedData = entries.map((data) => {
+                const [ id, task ] = data;
+                return { id: id, content: task }
+            })
+            console.log(gainedData)
+            const gainedSubjects: OneSubjectDataType[] = gainedData as OneSubjectDataType[];
+            console.log(gainedSubjects);
+            setSubjectList(gainedSubjects);
+        });
+    }, []);
 
     return (
         <StyledDiv margin='5% 0 0 0'>
@@ -47,10 +74,10 @@ const Frame8:React.FC = () => {
                             <StyledDiv flexGrow={20} width='95%' margin='0 10px 0 20px' backgroundColor='transparent'>
                                 <FlexBox flexDirection='row' flexWrap='wrap'>
                                     {
-                                        mockSubjects.map((item) => {
-                                            return  <StyledDiv enableShadow={true} margin='20px 20px 20px 0' isClickable={true} backgroundColor='#fefefe'>
+                                        subjectList.map((item) => {
+                                            return  <StyledDiv key={item.id} enableShadow={true} margin='20px 20px 20px 0' isClickable={true} backgroundColor='#fefefe'>
                                                         <HoverElement disableShadow={true} width='auto'>
-                                                            <FlexBox justifyContent='space-around' flexDirection='column' alignItems='center' width={ item.length*2.5 + 'em'} height='4em'>
+                                                            <FlexBox justifyContent='space-around' flexDirection='column' alignItems='center' width={ item.content.name.length*2.5 + 'em'} height='4em'>
                                                                 <StyledText size='2em' isClickable={true}>
                                                                     {item}
                                                                 </StyledText>
