48e614178fd46948bdd738f51eb64b732e81e97b
b1019116 <b1019116@fun.ac.jp>
2021-05-08 12:52:58 +0900

    科目追加実装中

--- src/components/Frame9.tsx
+++ src/components/Frame9.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlexBox, StyledDiv, StyledButton, StyledText, StyledInput } from './StyledComponents';
 import firebase, { database } from '../firebase';
+import { OneSubjectType } from '../DataTypes/SubjectTypes';
 
 type Frame9Props = {
     closeFrame9: () => void;
 }
 
 const Frame9: React.FC<Frame9Props> = (props) => {
+    const [ newSubjectName, setNewSubjectName ] = useState('');
+    const [ isSubmittable, setIsSubmittable ] = useState(false);
+    const [ isWarning, setIsWarning ] = useState(false);
+
     const { closeFrame9 } = props;
 
-    const 
+    const subjectRef = database.ref('subject');
+
+    const onChangeSubjectNameInput = (value: string) => {
+        setNewSubjectName(value);
+    }
+
+    const onSubmit = () => {
+        if(!isSubmittable){
+            setIsWarning(true);
+            return;
+        }
+        const user = firebase.auth().currentUser;
+        if(!user) return;
+        const userId = user.uid;
+        if(!userId) return;
+        const currentTimeStamp = new Date().toString();
+        const subjectListRef = subjectRef.child(userId);
+        const newSubject: OneSubjectType = {
+            name: newSubjectName,
+            memo: '',
+            create_at: currentTimeStamp,
+            update_at: currentTimeStamp
+        }
+        subjectListRef.push(newSubject);
+    }
+
+    const CheckIsSubmittable = () => {
+        if(newSubjectName === ''){
+            setIsSubmittable(false);
+        } else{
+            setIsSubmittable(true);
+        }
+    }
+
+    useEffect(()=>{
+        CheckIsSubmittable();
+    }, [newSubjectName]);
 
     return (
         <StyledDiv  margin='10% auto 0 auto'
@@ -40,7 +81,7 @@ const Frame9: React.FC<Frame9Props> = (props) => {
                                     placeholder='科目を入力してください。'/>
                 </StyledDiv>
                 <StyledDiv flexGrow={2} width='50%'>
-                    <StyledButton width='100%' height='2.0em' backgroundColor='#87cefa' fontSize='1.5em' fontWeight='bold' >
+                    <StyledButton onClick={()=>{ onSubmit() }} width='100%' height='2.0em' backgroundColor={isSubmittable ? '#87cefa' : 'd2d2d2'} fontColor={isSubmittable ? '#000000' : '#fefefe'} fontSize='1.5em' fontWeight='bold' >
                         登録
                     </StyledButton>
                 </StyledDiv>
