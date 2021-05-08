b6a0bf1b74edbe29b7ddcf5ab05b18fc9fd21d9e
soar1 <soarshizu@gmail.com>
2021-05-08 22:09:46 +0900

    [rebase]develop

--- src/components/Frame11.tsx
+++ src/components/Frame11.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
-import firebase from '../firebase';
+import React, { useState, useEffect} from 'react';
+import firebase, { database } from '../firebase';
 import { AbsoluteBox, FlexBox, HoverElement, RelativeBox, StyledButton, StyledDiv, StyledText, FixedBox} from './StyledComponents';
 import { Redirect } from 'react-router-dom';
 import HamburgerMenuButton from './HamburgerMenuButton';
 import Frame7 from './Frame7';
+import { OneSubjectDataType } from '../DataTypes/SubjectTypes';
 
 const Frame11:React.FC = () => {
 
     const mockSubjects = ['現代文', '古文', '漢文', '物理', '化学', '生物', '数学IA', '数学IIB', '数学III', '地理A', '地理B', '世界史A', '世界史B', '日本史A', '日本史B', '現代社会', '倫理', '家庭', '体育', '保健', '情報科学', 'アルゴリズムとデータ構造', 'プロジェクト学習'];
 
     const [isOpeningFrame7, setIsOpeningFrame7] = useState(false);
+    const [isOpeningFrame14, setIsOpeningFrame14] = useState(false);
+    const [ subjects, setSubjects ] = useState([] as OneSubjectDataType[]);
+    const [selectedSubject, setSelectedSubject] = useState('');
+    const subjectRef = database.ref('subject');
 
     const openFrame7 = () =>{
         setIsOpeningFrame7(true);
@@ -19,10 +24,45 @@ const Frame11:React.FC = () => {
         setIsOpeningFrame7(false);
     }
 
-    const handleClickMatrix = (day: number, period: number) => {
-        console.log('(' + day +  ', ' + period + ') clicked');
+    const openFrame14 = (selectedMemo?:string) =>{
+        setIsOpeningFrame7(true);
     }
 
+    const closeFrame14 = () =>{
+        setIsOpeningFrame7(false);
+    }
+
+    /*const handleClickMatrix = (day: number, period: number) => {
+        console.log('(' + day +  ', ' + period + ') clicked');
+    }*/
+    
+    useEffect(()=>{
+        // ユーザーの情報を取得。それぞれnullチェックを行う。
+        const user = firebase.auth().currentUser;
+        if(!user) return;
+        const userId = user.uid;
+        if(!userId) return;
+        // pathを指定して、データへの参照を取得。
+        const listRef = subjectRef.child(userId);
+        if(!listRef) return;
+        // onメソッドは、参照先のデータを取得するメソッド。
+        listRef.on('value', (snapshot) => {
+            const subjects = snapshot.val();
+            if(subjects === null) return;
+            // entriesには、オブジェクトのキー値（自動生成されたもの）と、中身の値（オブジェクト）のペアが配列になって返ってくるようだ。
+            const entries = Object.entries(subjects);
+            // entriesをオブジェクトに直す
+            const gainedData = entries.map((data) => {
+                const [ id, subject ] = data;
+                return { id: id, content: subject }
+            })
+            // stateにデータを保持
+            // gainedDataは型が決まっていないので、DataTypeにキャストする。
+            const gainedSubjects: OneSubjectDataType[] = gainedData as OneSubjectDataType[];
+            setSubjects(gainedSubjects);
+        })
+    }, []);
+    
 
     const render = () => {
         if(firebase.auth().currentUser) {
@@ -56,12 +96,12 @@ const Frame11:React.FC = () => {
                                     <StyledDiv flexGrow={20} width='95%' margin='0 10px 0 20px' backgroundColor='transparent'>
                                         <FlexBox flexDirection='row' flexWrap='wrap'>
                                             {
-                                                mockSubjects.map((item) => {
+                                                subjects.map((item) => {
                                                     return  <StyledDiv enableShadow={true} margin='20px 20px 20px 0' isClickable={true} backgroundColor='#fefefe'>
                                                                 <HoverElement disableShadow={true} width='auto'>
-                                                                    <FlexBox justifyContent='space-around' flexDirection='column' alignItems='center' width={ item.length*2.5 + 'em'} height='4em'>
-                                                                        <StyledText size='2em' isClickable={true}>
-                                                                            {item}
+                                                                    <FlexBox justifyContent='space-around' flexDirection='column' alignItems='center' width={ item.content.name.length*2.5 + 'em'} height='4em'>
+                                                                        <StyledText onClick={()=>{ openFrame14()}} size='2em' isClickable={true}>
+                                                                        {item.content.name}
                                                                         </StyledText>
                                                                     </FlexBox>
                                                                 </HoverElement>
