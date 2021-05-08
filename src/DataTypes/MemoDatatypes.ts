71b3f2707a93fc3747d903a17b1a3090c182df8e
soar1 <soarshizu@gmail.com>
2021-05-08 15:25:04 +0900

    [rebase]develop

--- src/DataTypes/MemoDatatypes.ts
+++ src/DataTypes/MemoDatatypes.ts
@@ -1,7 +1,12 @@
-export type OneSubjectType = {
+export type MemoDataType = {
+    id: string;
+    content: MemoType;
+}
+
+export type MemoType = {
     user_id: string;
-    title:;
-    content:;
-    create__at:;
-    update_at:;
+    title: string;
+    content: string;
+    create__at: string;
+    update_at: string;
 }
